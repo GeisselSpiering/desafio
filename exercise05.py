82e923e468a280e230bd80fb0805ea90790a9bf1
Geissel Spiering <88937387+GeisselSpiering@users.noreply.github.com>
2023-05-01 12:20:05 -0300

    Update exercise05.py

--- exercise05.py
+++ exercise05.py
@@ -1,4 +1,7 @@
-def invert(text):
-    return text[::-1]
-print(invert('INVERTER'))
-print(invert('INVERTER A ORDEM DESSE TEXTO'))
+string = "INVERTER O TEXTO"
+string_invertida = ""
+
+for i in range(len(string) - 1, -1, -1):
+    string_invertida += string[i]
+
+print(string_invertida)
