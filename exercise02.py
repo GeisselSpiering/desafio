ccb494177e589bcce064f4f2c28960b9ada8c2e1
Geissel Spiering <88937387+GeisselSpiering@users.noreply.github.com>
2023-05-01 12:09:23 -0300

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,1 +1,13 @@
+num = int(input("\nDigite um número para verificar se pertence à sequência de Fibonacci: "))
 
+fibonacci = [0, 1]
+while fibonacci[-1] < num:
+    fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+if num in fibonacci:
+    print(f"O número {num} faz parte da sequência de Fibonacci!")
+else:
+    print(f"O número {num} não faz parte da sequência de Fibonacci.")
+
+print("Sequência de Fibonacci:")
+print(fibonacci[:-1])
